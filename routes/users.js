9b28552fe617172798cb613dec93a6452ad98ae2
myyusuf <myyusuf1911@gmail.com>
2017-06-19 13:02:26 +0700

    Update and delete  User

--- routes/users.js
+++ routes/users.js
@@ -13,9 +13,40 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/:id', function(req, res, next) {
+  const id = req.params.id;
+  models.User.findOne({
+    where: { id },
+  })
+  .then((user) => {
+    res.json({ user });
+  });
+});
+
 router.post('/', function(req, res, next) {
   const userData = req.body;
   models.User.create(userData)
+  .then((result) => {
+    res.json({ result });
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+  const id = req.params.id;
+  models.User.destroy({
+    where: { id },
+  })
+  .then((result) => {
+    res.json({ result });
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+  const id = req.params.id;
+  const userData = req.body;
+  models.User.update(
+    userData,
+    { where: { id } })
   .then((result) => {
     res.json({ result });
   });
