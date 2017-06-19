7bd9e85f52aa9895234da1d23d33beda251cf221
erwinwahyura <you@example.com>
2017-06-19 15:55:23 +0700

    add auth, and routing, all works well

---
+++ controllers/auth.js
@@ -0,0 +1,67 @@
+var db = require('../models')
+var jwt = require('jsonwebtoken')
+
+var authAdmin = function(req, res, next) {
+  let token = req.headers.token
+  if(token) {
+    jwt.verify(token, 'Myscret24@!!createisds!okdoskdokaDONTcop!!', (err, decoded) => {
+      if(!err) {
+        if(decoded.role.toLowerCase() == "admin") {
+          next()
+        } else {
+          res.send({msg: 'you are not authorized!'})
+        }
+      } else {
+        res.send(err)
+      }
+    })
+  } else {
+    res.send({msg: 'Not logged in'})
+  }
+}
+
+var authUser = function(req, res, next) {
+  let token = req.headers.token
+  if(token) {
+    jwt.verify(token, 'Myscret24@!!createisds!okdoskdokaDONTcop!!', (err, decoded) => {
+      if(!err) {
+        if(decoded.role.toLowerCase() == "user") {
+          next()
+        } else {
+          res.send({msg: 'you are not authorized!'})
+        }
+      } else {
+        res.send(err)
+      }
+    })
+  } else {
+    res.send({msg: 'Not logged in'})
+  }
+}
+
+var all = function(req, res, next) {
+  let token = req.headers.token
+  if(token) {
+    jwt.verify(token, 'Myscret24@!!createisds!okdoskdokaDONTcop!!', (err, decoded) => {
+      if(!err) {
+        if(decoded.role.toLowerCase() == "user" || decoded.role.toLowerCase() == "admin") {
+          next()
+        } else {
+          res.send({msg: 'you are not authorized!'})
+        }
+      } else {
+        res.send(err)
+      }
+    })
+  } else {
+    res.send({msg: 'Not logged in'})
+  }
+}
+
+
+
+module.exports = {
+  authUser,
+  authAdmin,
+  all
+}
